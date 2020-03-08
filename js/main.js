f4e8d5d24a1fd376c642646c90e5afccb6643265
Scott <scottmcintire90@gmail.com>
2020-03-08 13:05:44 -0700

    add js to skills blurbs

--- js/main.js
+++ js/main.js
@@ -5,21 +5,32 @@ $(document).ready(function(){
       $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
   });
 
-  const addExpCircles = (full, empty, selector) => {
+  $('.skills > .col-sm-3').hover(function() {
+    $(this).find('.blurb').animate({opacity: 1}, 300)
+    }, function() {
+    $(this).find('.blurb').animate({opacity: 0}, 300)
+  });
 
+  // $('#development').hover(function () {
+  //   $("#development .blurb").css("display", "block").fadeIn('slow');
+  //   $("#development .blurb").fadeIn('slow');
+  // },function () {
+  //   $("#development .blurb").css("display", "none");
+  // });
+
+  // Experience circles
+  const addExpCircles = (full, empty, selector) => {
     // Add full circles
     for(let i=0; i<full; i++) {
       $(`${selector} .exp-circles`).append('<i class="fa fa-circle" style="color:#4f9cca;"></i>')
       console.log(`${selector} .exp-circles`)
     }
-
     // Add empty circles
     for(let i=0; i<empty; i++) {
       $(`${selector} .exp-circles`).append('<i class="fa fa-circle" style="color:#c2d0d6;"></i>')
       console.log(`${selector} .exp-circles`)
     }
   }
-
   addExpCircles(9, 1, "#html5");
   addExpCircles(9, 1, "#cssSass");
   addExpCircles(8, 2, "#javascript");
