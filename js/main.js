55177a975cc13b0085956936f51ea256814ed760
Scott McIntire <scottmcintire90@gmail.com>
2020-03-11 23:16:41 -0700

    add country color change and name change

--- js/main.js
+++ js/main.js
@@ -5,18 +5,48 @@ $(document).ready(function(){
       $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
   });
 
+  var objectElem = document.querySelector("object");
+  var svg;
+
+  objectElem.addEventListener("load", function () {
+    contentDoc = objectElem.contentDocument;
+    svg = contentDoc.documentElement;
+    let countries = ["US", "MX", "CR", "BR", "NO", "CN", "JP"];
+    
+    (function colorCountry (n) {
+
+      $(svg.children[countries[n]]).toggleClass('black').delay(4000).queue(function(next){
+            $(this).toggleClass('normal-background');
+            next();
+      });
+      n++
+      if (n <= 20) {
+        setTimeout(colorCountry, 4000, n);
+      }
+    }(0));
+    
+  });
+
   $('.skills > .col-sm-3').hover(function() {
     $(this).find('.blurb').animate({opacity: 1}, 300)
     }, function() {
     $(this).find('.blurb').animate({opacity: 0}, 300)
   });
 
-  // $('#development').hover(function () {
-  //   $("#development .blurb").css("display", "block").fadeIn('slow');
-  //   $("#development .blurb").fadeIn('slow');
-  // },function () {
-  //   $("#development .blurb").css("display", "none");
-  // });
+ let welcomeMsg = ["Welcome", "Bienvenido", "Pura Vida", "Bem-vindo", "Velkommen", "Ni Hao", "Irasshaimase"]
+
+  var counter = 0;
+  var elem = document.getElementById("welcome");
+  var inst = setInterval(change, 2500);
+
+  function change() {
+    elem.innerHTML = welcomeMsg[counter];
+    counter++;
+    if (counter >= welcomeMsg.length) {
+      counter = 0;
+      // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+    }
+  }
 
   // Experience circles
   const addExpCircles = (full, empty, selector) => {
