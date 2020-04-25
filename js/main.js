ca0b888057c7994e8881bcfe00ef01ebb13ef0d1
Scott <scottmcintire90@gmail.com>
2020-04-25 13:06:00 -0700

    add new assets to about

--- js/main.js
+++ js/main.js
@@ -3,12 +3,32 @@ $(document).ready(function(){
   /* ---------- Smooth scroll arrow ---------- */
   $( "a.scrollLink" ).click(function( event ) {
       event.preventDefault();
-      $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
+      $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 800);
   });
-  
+
   $('.ui .item').on('click', function() {
     $('.ui .item').removeClass('active');
     $(this).addClass('active');
+  });
+
+  // Menu Position
+  let menuPos = $('#mainMenu').position().top;
+  let lastPos = 0;
+
+  $(window).on('scroll', function() {
+    let pos = $(window).scrollTop();
+
+    if (pos >= menuPos + $('#mainMenu').height() && lastPos < pos) {
+      $('#mainMenu').addClass('fixed');
+      // $('#mainMenu').css("display", "flex")
+    }
+    if (pos < menuPos && lastPos > pos) {
+      $('#mainMenu').removeClass('fixed');
+    }
+    lastPos = pos;
+  
+
+   
   });
 
   /* ---------- SVG Animation ---------- */
