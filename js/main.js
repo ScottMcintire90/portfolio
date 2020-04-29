00164fd801ef94b0bdd1187e587bf764f76c7eaf
Scott McIntire <scottmcintire90@gmail.com>
2020-04-29 01:37:22 -0700

    update hero

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   /* ---------- Smooth scroll arrow ---------- */
   $( "a.scrollLink" ).click(function( event ) {
       event.preventDefault();
+      window.setTimeout(function(){$('#mainMenu').addClass('fixed');}, 75);
+      $('.menu a').removeClass('active');
+      $('.menu a:nth-child(2)').addClass('active');
       $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 800);
   });
 
@@ -32,64 +35,64 @@ $(document).ready(function(){
   });
 
   /* ---------- SVG Animation ---------- */
-  var objectElem = document.querySelector("object");
-  var svg;
+  // var objectElem = document.querySelector("object");
+  // var svg;
 
-  objectElem.addEventListener("load", function () {
-    contentDoc = objectElem.contentDocument;
-    svg = contentDoc.documentElement
-    let welcomeMsg = [
-      ["W", "E", "L", "C", "O", "M", "E"],
-      ["B", "I", "E", "N", "V", "E", "N", "I", "D", "O"],
-      ["B", "E", "M", "-", "V", "I", "N", "D", "O"],
-      ["V", "E", "L", "K", "O", "M", "M", "E", "N"],
-      ["こ", "ん", "に", "ち", "は"],
-      ["W", "I", "L", "L", "K", "O", "M", "M", "E", "N"],
-      ["B", "O", "N", "J", "O", "U", "R"]
-    ];  
+  // objectElem.addEventListener("load", function () {
+  //   contentDoc = objectElem.contentDocument;
+  //   svg = contentDoc.documentElement
+  //   let welcomeMsg = [
+  //     ["W", "E", "L", "C", "O", "M", "E"],
+  //     ["B", "I", "E", "N", "V", "E", "N", "I", "D", "O"],
+  //     ["B", "E", "M", "-", "V", "I", "N", "D", "O"],
+  //     ["V", "E", "L", "K", "O", "M", "M", "E", "N"],
+  //     ["こ", "ん", "に", "ち", "は"],
+  //     ["W", "I", "L", "L", "K", "O", "M", "M", "E", "N"],
+  //     ["B", "O", "N", "J", "O", "U", "R"]
+  //   ];  
 
-    (function updateWelcomeMsg (n) {
-      document.getElementById("welcome").innerHTML = ""
+  //   (function updateWelcomeMsg (n) {
+  //     document.getElementById("welcome").innerHTML = ""
 
-      var greeting = welcomeMsg[n]
+  //     var greeting = welcomeMsg[n]
 
-      for (let i=0; i<12; i++) { 
-        updateWelcome(i);
-      } 
+  //     for (let i=0; i<12; i++) { 
+  //       updateWelcome(i);
+  //     } 
 
-      function updateWelcome(i) { 
-        setTimeout(function() { 
-          $("#welcome").append(greeting[i])
-        }, 40 * i); 
-      } 
+  //     function updateWelcome(i) { 
+  //       setTimeout(function() { 
+  //         $("#welcome").append(greeting[i])
+  //       }, 40 * i); 
+  //     } 
 
-      n++
+  //     n++
       
-      if (n === welcomeMsg.length) {
-        n=0;
-      }
+  //     if (n === welcomeMsg.length) {
+  //       n=0;
+  //     }
 
-      setTimeout(updateWelcomeMsg, 4000, n);
-    }(0));
-  });
+  //     setTimeout(updateWelcomeMsg, 4000, n);
+  //   }(0));
+  // });
 
-  /* ---------- Skills ---------- */
-  $('.skills > .col-sm-3').hover(function() {
-    $(this).find('.blurb').animate({opacity: 1}, 300)
-    }, function() {
-    $(this).find('.blurb').animate({opacity: 0}, 300)
-  });
+  // /* ---------- Skills ---------- */
+  // $('.skills > .col-sm-3').hover(function() {
+  //   $(this).find('.blurb').animate({opacity: 1}, 300)
+  //   }, function() {
+  //   $(this).find('.blurb').animate({opacity: 0}, 300)
+  // });
 
 
   /* ---------- Experience circles ---------- */
   const addExpCircles = (full, empty, selector) => {
     // Add full circles
     for(let i=0; i<full; i++) {
-      $(`${selector} .exp-circles`).append('<i class="fa fa-circle" style="color:#4f9cca;"></i>')
+      $(`${selector} .exp-circles`).append('<i class="fa fa-circle full-circle"></i>')
     }
     // Add empty circles
     for(let i=0; i<empty; i++) {
-      $(`${selector} .exp-circles`).append('<i class="fa fa-circle" style="color:#c2d0d6;"></i>')
+      $(`${selector} .exp-circles`).append('<i class="fa fa-circle empty-circle"></i>')
     }
   }
 
