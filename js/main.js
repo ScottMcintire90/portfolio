07e6c876225fe02dfcb3fa2701e1586c9f186493
Scott McIntire <scottmcintire90@gmail.com>
2020-05-12 14:50:18 -0700

    remove some images and sections

--- js/main.js
+++ js/main.js
@@ -1,37 +1,81 @@
 $(document).ready(function(){
 
   /* ---------- Smooth scroll arrow ---------- */
-  $( "a.scrollLink" ).click(function( event ) {
-      event.preventDefault();
-      window.setTimeout(function(){$('#mainMenu').addClass('fixed');}, 00);
-      $('.menu a').removeClass('active');
-      $('.menu a:nth-child(2)').addClass('active');
-      $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 800);
+  $( "a.scrollLink" ).click(function() {
+      let id = $(this).attr('href');
+      $('html, body').animate({
+          scrollTop: $(id).offset().top
+        },
+        500
+      );
   });
 
-  $('.ui .item').on('click', function() {
-    $('.ui .item').removeClass('active');
-    $(this).addClass('active');
+  $('#mainMenu .item').on('click', function() {
+    $('#mainMenu .active').removeClass('active');
+    window.setTimeout(() => {
+      $(this).addClass('active');
+    }, 500)
   });
 
-  // Menu Position
   let menuPos = $('#mainMenu').position().top;
   let lastPos = 0;
 
   $(window).on('scroll', function() {
     let pos = $(window).scrollTop();
+    let pos2 = pos + 50;
 
     if (pos >= menuPos + $('#mainMenu').height() && lastPos < pos) {
       $('#mainMenu').addClass('fixed');
-      // $('#mainMenu').css("display", "flex")
     }
     if (pos < menuPos && lastPos > pos) {
       $('#mainMenu').removeClass('fixed');
     }
+
     lastPos = pos;
   
-
-   
+    if (pos2 > $('#home').offset().top) {
+      addClassActive('#home');
+    }
+    if (pos2 > $('#about').offset().top) {
+      addClassActive('#about');
+    }
+    if (pos2 > $('#skills').offset().top) {
+      addClassActive('#skills');
+    }
+    if (pos2 > $('#projects').offset().top) {
+      addClassActive('#projects');
+    }
+    if (
+      pos2 > $('#contact').offset().top ||
+      pos + $(window).height() === $(document).height()
+    ) {
+      addClassActive('#contact');
+    }
   });
+
+  function addClassActive(id) {
+    $('#mainMenu .active').removeClass('active');
+    $('#mainMenu')
+      .find('[href="' + id + '"]')
+      .addClass('active');
+  }
+
+  // Project Info
+  $(".project-img").hover(
+    function() {
+    $(this).siblings(".project-info").addClass('test12');
+    },
+    function() {
+      $(this).siblings(".project-info").removeClass('test12');
+    }
+  )
+  $(".project-info").hover(
+    function() {
+    $(this).addClass('test12');
+    },
+    function() {
+      $(this).removeClass('test12');
+    }
+  )
 
 });
